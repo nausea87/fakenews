d4e031c8a3dfb751d7e2dcbf5c2092457d30ab92
Erik <62840687+nausea87@users.noreply.github.com>
2020-10-19 10:22:48 +0200

    add js

--- data.php
+++ data.php
@@ -2,5 +2,18 @@
 
 declare(strict_types=1);
 
+$news =
+[
+    [
+        'title' => 'Cats in need',
+        'author' => 'Cati',
+        'published_date' => '2020-03-12 12:12',
+        'content' =>
+        'likes' =>
+        'comments' =>
+        'img' =>
+    ]
+]
+
 // This is the file where you can keep your data arrays such as articles and
 // authors.
