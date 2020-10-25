cf9feda5904750b41141f809cf6c4c9f25894814
Erik <62840687+nausea87@users.noreply.github.com>
2020-10-25 14:44:03 +0100

    stored arrays

--- data.php
+++ data.php
@@ -1,69 +1,146 @@
 <?php
 
-declare(strict_types=1); //Devide into further PHP?
-$data = [
+declare(strict_types=1);
+
+$authors = [
+    ['name' => 'Björk Janlund', 'id' => 1],
+    ['name' => 'Annie Rööf', 'id' => 2],
+    ['name' => 'Immie Jåkesson', 'id' => 3],
+    ['name' => 'Tonald Drump', 'id' => 4],
+    ['name' => 'Cill Blinton', 'id' => 5],
+
+];
+
+$articles = [
+
     [
-        'titles' =>  [
-            '33 year old Glenneth finds record huge mushroom',
-            '2',
-            '3',
-            '4',
-            '5',
-            '6',
-            '7',
-            '8',
-            '9',
-            '10',
-        ],
-
-        'stories' => [
-            'I was like...Herrejävlar, that/s huge. I mean, look at it! I/m gonna make fuck
-            tons of...Ah, svampmacka you know? Bread in the stekpanna, butter and shrooms.',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-        ],
-
-        'authors' => [
-            'Björk Janlund',
-            'Johnny Jokkmokk',
-            'Steffe S Steffesson',
-            'Ronny Connysson',
-            'Glenn G Glennsson',
-        ],
-
-        'published_date' => [
-            '2020-03-12 12:12',
-            '2020-03-13 13:37',
-            '2020-04-02 09:12',
-            '2019-05-11 11:35',
-            '2019-12-24 23:01',
-            '2020-03-12 12:12',
-            '2020-03-12 12:12',
-            '2020-03-12 12:12',
-            '2020-03-12 12:12',
-            '2020-03-12 12:12',
-        ],
-
-        'likes' => [
-            '69.69',
-            '69.69',
-            '69.69',
-            '69.69',
-            '69.69',
-            '69.69',
-            '69.69',
-            '69.69',
-            '69.69',
-            '69.69',
-        ],
+        'title' => 'Man from Gothenburg finds worlds largest mushroom',
+        'published_date' => '2020-10-18 06:59',
+        'content' => 'I was like...Herrejävlar, that/s a huge mushroom änna. I mean, look at it! I/m gonna make f**k
+            tons of...Ah, svampmacka you know? Bread in the stekpanna, butter and shrooms.
+            Some salt och pepper. Very tasy.',
+        'id' => 1,
+        'likes' => '69',
+        'author' => 'Björk Janlund', //Mushroom (1)
     ],
+
+    [
+        'title' => 'Henrik, 33 about his latest trip to Greece',
+        'published_date' => '2020-10-18 08:29',
+        'content' => 'Yeah I like Greece you know, very hot and also good Feta you know?
+            I recommend this place alot, you can have beer in the pool, but there/s en jävla massa
+            getingar your know? Getings? They fly around and you might get sticken.
+            Once there was a huge fu**ing geting who flew away with my bacon from a cesarsallad.
+            Rätt coolt ändå, because the bacon was much bigger than the geting.',
+        'id' => 1,
+        'likes' => '1.3',
+        'author' => 'Björk Janlund',
+    ],
+
+    [
+        'title' => 'Carl-Jennys guide to the stars',
+        'published_date' => '2020-10-18 11:09',
+        'content' => 'Yeah I usually go to Thailand to look at space. Pretty cool.
+            I mean, imagine Russia. Pretty jävla big huh? Then you think about space...
+            Much bigger. Much, much bigger. Probably a thousand times bigger than Russia I reckon.',
+        'id' => 2,
+        'likes' => '20',
+        'author' => 'Annie Rööf',
+    ],
+
+    [
+        'title' => 'Henrik goes on a boat trip',
+        'published_date' => '2020-10-18 12:59',
+        'content' => 'A, nämen I like boats I guess. In greece they have some boats. What more is there to say?',
+        'id' => 2,
+        'likes' => '1337',
+        'author' => 'Annie Rööf',
+    ],
+
+    [
+        'title' => 'Sweden to launch nuclear weapons',
+        'published_date' => '2020-10-18 14:55',
+        'content' => 'Next month, Sweden is expected to finish their work on their first nuclear missile.
+            According to prime minister Stefan Lööven one big factor in this project has been the growing
+            threat of the neighbouring country on Norway',
+        'id' => 3,
+        'likes' => '29',
+        'author' => 'Immie Jåkesson',
+    ],
+
+    [
+        'title' => 'Sunbeams and stars',
+        'published_date' => '2020-10-18 18:49',
+        'content' => 'Vanquish the impossible star stuff harvesting star light dispassionate extraterrestrial 
+         observer stirred by starlight tingling of the spine rich in heavy atoms. Preserve and cherish that pale 
+         blue dot rings of Uranus something incredible is waiting to be known the ash of stellar alchemy network of wormholes 
+         not a sunrise but a galaxyrise. Made in the interiors of collapsing stars courage of our questions at the edge of 
+         forever preserve and cherish that pale blue dot with pretty stories for which there/s little good evidence a mote 
+         of dust suspended in a sunbeam and billions upon billions upon billions upon billions upon billions upon billions upon billions.',
+        'id' => 3,
+        'likes' => '27',
+        'author' => 'Immie Jåkesson',
+    ],
+
+    [
+        'title' => 'Ashes of gravity',
+        'published_date' => '2020-10-18 22:03',
+        'content' => 'Drake Equation finite but unbounded encyclopaedia galactica hearts of the stars cosmos vanquish the 
+        impossible. Made in the interiors of collapsing stars inconspicuous motes of rock and gas laws of physics muse about 
+        circumnavigated of brilliant syntheses. Courage of our questions emerged into consciousness a still more glorious dawn 
+        awaits muse about the ash of stellar alchemy a very small stage in a vast cosmic arena. The only home we/ve ever known 
+        hydrogen atoms the ash of stellar alchemy vastness is bearable only through love gathered by gravity made in the interiors of collapsing stars.
+        Hydrogen atoms dream of the mind/s eye Cambrian explosion the sky calls to us courage of our questions at the edge of forever. 
+        A still more glorious dawn awaits two ghostly white figures in coveralls and helmets are softly dancing Sea of Tranquility from 
+        which we spring Orion/s sword rich in heavy atoms. The ash of stellar alchemy a very small stage in a vast cosmic arena rings of 
+        Uranus extraordinary claims require extraordinary evidence ship of the imagination citizens of distant epochs and billions upon 
+        billions upon billions upon billions upon billions upon billions upon billions.',
+        'id' => 4,
+        'likes' => '25',
+        'author' => 'Tonald Drump',
+    ],
+
+    [
+        'title' => 'Trump to join PETA',
+        'published_date' => '2020-10-19 04:13',
+        'content' => 'I think my strongest asset maybe by far is my temperament. I have a placeholding temperament. I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. 
+        Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites. When other websites give you text, they’re not sending the best. They’re not sending you, they’re sending words that have lots of problems and they’re bringing those problems with us. They’re bringing mistakes. They’re bringing misspellings. They’re typists… And some, I assume, are good words. An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.
+        The concept of Lorem Ipsum was created by and for the Chinese in order to make U.S. design jobs non-competitive. You know, it really doesn’t matter what you write as long as you’ve got a young, and beautiful, piece of text. We have so many things that we have to do better... and certainly ipsum is one of them.
+        Despite the constant negative ipsum covfefe. Does everybody know that pig named Lorem Ipsum? She/s a disgusting pig, right?
+        Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites. An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud. It’s about making placeholder text great again. That’s what people want, they want placeholder text to be great again.',
+        'id' => 4,
+        'likes' => '3',
+        'author' => 'Tonald Drump',
+    ],
+
+    [
+        'title' => 'Trump attacks Molly, 5',
+        'published_date' => '2020-10-19 06:17',
+        'content' => 'Some people have an ability to write placeholder text... It/s an art you/re basically born with. You either have it or you don/t. Look at these words. Are they small words? And he referred to my words - if they/re small, something else must be small. I guarantee you there/s no problem, I guarantee.',
+        'id' => 5,
+        'likes' => '12',
+        'author' => 'Cill Blinton',
+    ],
+
+    [
+        'title' => 'Pickles to cause cancer, new study claims',
+        'published_date' => '2020-10-19 14:12',
+        'content' => '',
+        'id' => 5,
+        'likes' => '55',
+        'author' => 'Cill Blinton',
+    ],
+
+
+
+
+
+
+
+
+
+
+
 ];
     
 
