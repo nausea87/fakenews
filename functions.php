cef6aeb94b7f4f23e58657c1cb427e2e1b905454
Erik <62840687+nausea87@users.noreply.github.com>
2020-10-31 11:56:20 +0100

    added final comments

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ function getArticleById(array $articles, int $id): array
     foreach ($articles as $article) {
         if ($article['id'] === $id) {
             return $article;
-        } /* Working properly */
+        } /* Creating article $id from ['id'] and calling the variable $id in index.php */
     }
 }
 
@@ -19,6 +19,7 @@ function date_compare(array $element1, array  $element2): int
     $datetime1 = strtotime($element1['published_date']);
     $datetime2 = strtotime($element2['published_date']);
     return $datetime2 - $datetime1;
+    /* To sort articles from time of publishing in 'usort' in index.php */
 }
 
 function shortenText(string $text, $chars = 300): string
@@ -28,5 +29,6 @@ function shortenText(string $text, $chars = 300): string
     $text = substr($text, 0, strrpos($text, ' '));
     $text = $text . "...";
     return $text;
-    /* Might remove temporary due to button not fixed */
+    /* Might remove temporary due to button not fixed as I want yet */
+    /* Function to be implemented again after learning more. */
 }
