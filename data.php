21ee3de2c6bf0a31c017b102f40756ee39d99d98
Erik <62840687+nausea87@users.noreply.github.com>
2020-11-01 13:11:19 +0100

    More exclusive content, Sunday 01/11

--- data.php
+++ data.php
@@ -9,7 +9,9 @@ $articles = [
         'published_date' => '2020-10-30 06:59',
         'content' => 'I was like...Herrejävlar, that is a huge mushroom änna. I mean, look at it! I am gonna make f**k
             tons of...Ah, svampmacka you know? Bread in the stekpanna, butter and shrooms.
-            Some salt och pepper. Very tasy.',
+            Some salt och pepper. Very tasy. Have you tried svampmacka? You should. It is the best.
+            Wait, did you photoshop this picture? That is not the jacket I wore...That is not even me
+            on that picture. What the hell mate?',
         'id' => 1,
         'likes' => '69',
         'dislikes' => '37',
@@ -20,11 +22,15 @@ $articles = [
     [
         'title' => 'Henrik, 33 about his latest trip to Greece',
         'published_date' => '2020-10-20 08:29',
-        'content' => 'Yeah I like Greece you know, very hot and also good Feta you know?
+        'content' => 'Yeah I like Greece you know, very hot and also good fetaost you know?
             I recommend this place alot, you can have beer in the pool, but there/s en jävla massa
             getingar your know? Getings? They fly around and you might get sticken.
             Once there was a huge fu**ing geting who flew away with my bacon from a cesarsallad.
-            Rätt coolt ändå, because the bacon was much bigger than the geting.',
+            Rätt coolt ändå, because the bacon was much bigger than the geting.
+            By the way, Am I getting paid for being in the news? No? Why not not?
+            Ah, never mindmind. What should I write now then? I like Detroit Red Wings, anything
+            about that you can put in the newspaper? No? What do you mean everyone likes Detroit
+            Red Wings?',
         'id' => 1,
         'likes' => '3',
         'dislikes' => '9',
@@ -37,7 +43,10 @@ $articles = [
         'published_date' => '2020-10-23 11:09',
         'content' => 'Yeah I usually go to Thailand to look at space. Pretty cool.
             I mean, imagine Russia. Pretty jävla big huh? Then you think about space...
-            Much bigger. Much, much bigger. Probably a thousand times bigger than Russia I reckon.',
+            Much bigger. Much, much bigger. Probably a thousand times bigger than Russia I reckon.
+            Nah, yeah nah that is just my guess. What do I know. Have you been in space? Would be pretty
+            epic right? Maybe in the future we can travel around up there as we please. Hell yeah.
+            Oh, another photoshopped picture of some bloke again, huh? Jeez. Many creativity, much wow.',
         'id' => 2,
         'likes' => '20',
         'dislikes' => '420',
@@ -48,7 +57,13 @@ $articles = [
     [
         'title' => 'Henrik goes on a boat trip',
         'published_date' => '2020-10-22 12:59',
-        'content' => 'A, nämen I like boats I guess. In greece they have some boats. What more is there to say?',
+        'content' => 'A, nämen I like boats I guess. In greece they have some boats. What more is there to say?
+        If I usually wear this jacket whereever I travel? Yeah, well. ofcourse I do not. Some moron keeps Photoshopping me
+        into stupid pictures, like I wear the same jacket everywhere. You really think I would wear this when it is 30 something
+        degrees celcius outside? Hell no. Crikey, that is not even me on that picture. Look at me. Do I look like this beardy ginger
+        c**t? Exacly. Anyways, what is your opinion about flat-eartheners? As a sidenote I also like Grotesco, you know? Swedish comedy about
+        weird stuff. You know "Ett kilo mjöl"? Classic that one if you ask me. Anyway yeah, nah yeah at this point you probably know that all
+        of this garbage is just to fill this article with "content". Pretty cool tho if you read all of this. Gotta hand it to you. Cheers!',
         'id' => 2,
         'likes' => '1337',
         'dislikes' => '69',
@@ -61,7 +76,12 @@ $articles = [
         'published_date' => '2020-10-18 14:55',
         'content' => 'Next month, Sweden is expected to finish their work on their first nuclear missile.
             According to prime minister Stefan Lööven one big factor in this project has been the growing
-            threat of the neighbouring country of Norway',
+            threat of the neighbouring country of Norway
+            "-Yeah nah yeah, they had it comming." Stefan says. He also said that whoever wrote these articles
+            need to format them better, because they can be be difficult to read. I kind of agree, and I am working on it.
+            But now I am watching some video of Gordon Ramsay and I got a bit hungry, eventhough he always hates the food.
+            Yeah, whatever. Sweden to nuke Norway I guess. Overkill if you ask me.
+        ',
         'id' => 3,
         'likes' => '29',
         'dislikes' => '1',
@@ -72,10 +92,13 @@ $articles = [
     [
         'title' => '2021 to be postponed',
         'published_date' => '2020-10-18 18:49',
-        'content' => 'The global Covid situation world leaders
+        'content' => 'Yeah so, with the global Covid situation world leaders
         have now agreed on postponing 2021 until 2022. The news have been
         welcomed worldwide due to the general opinion of 2020 being total
-        crap.',
+        crap. Might aswell postpone it further in my honest opinion, but I am not the leader
+        am I? I just write these articles because it amuses me. I should also fix a picture for this article. 
+        And if there is a picture when you read this, I probably just forgot to remove this text.
+        Yeah so, anyway...2021 to be postponed. Rad.',
         'id' => 3,
         'likes' => '27',
         'dislikes' => '37',
@@ -88,7 +111,8 @@ $articles = [
         'published_date' => '2020-10-18 22:03',
         'content' => 'Due to next summer olympics games Kubb will be included as a sport.
         This was decided last week by the Olympic Committé in Borås, and the decicion has been
-        highly popular so far. The reason behind the inclusion is yet to be known.',
+        highly popular so far. The reason behind the inclusion is yet to be known. I must
+        say this is a bit of a skräll. Kubb? Really? Who would have thought?',
         'id' => 4,
         'likes' => '25',
         'dislikes' => '44',
@@ -97,17 +121,16 @@ $articles = [
     ],
 
     [
-        'title' => 'Trump to join PETA',
+        'title' => 'Top game review',
         'published_date' => '2020-10-19 04:13',
-        'content' => 'I think my strongest asset maybe by far is my temperament. I have a placeholding temperament. I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. 
-        Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites. When other websites give you text, they’re not sending the best. They’re not sending you, they’re sending words that have lots of problems and they’re bringing those problems with us. They’re bringing mistakes. They’re bringing misspellings. They’re typists… And some, I assume, are good words. An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.
-        The concept of Lorem Ipsum was created by and for the Chinese in order to make U.S. design jobs non-competitive. You know, it really doesn’t matter what you write as long as you’ve got a young, and beautiful, piece of text. We have so many things that we have to do better... and certainly ipsum is one of them.
-        Despite the constant negative ipsum covfefe. Does everybody know that pig named Lorem Ipsum? She/s a disgusting pig, right?
-        Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites. An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud. It’s about making placeholder text great again. That’s what people want, they want placeholder text to be great again.',
+        'content' => 'The game review of today sees 2020 bestseller "First Date Simlulator 98".
+        It has been called  "A masterpiece" and "Game changer" by many and we solely agree.
+        According to most people this game has it all. Top notch graphics, gameplay, sound and humour. 
+        We here at Fake News gives this game 13 out of a total 13 points. Get it now!',
         'id' => 4,
         'likes' => '3',
         'dislikes' => '15',
-        'img' => '',
+        'img' => 'images/game.jpg',
         'author' => 'Tonald Drump',
     ],
 
@@ -116,7 +139,9 @@ $articles = [
         'published_date' => '2020-10-19 06:17',
         'content' => 'This morning it was revealed to the rest of the world that former
         prime minister of Sweden, Olof Palme, is alive and well. Apparently he was never shot
-        but instead went into hiding after a bet "gone wrong", Palme claims',
+        but instead went into hiding after a bet "gone wrong", Palme claims.
+        Two years ago BetnowX offered to odds 1 to 31.553.222 which could mean that a few
+        people who made this bet are now very wealthy.',
         'id' => 5,
         'likes' => '12',
         'dislikes' => '4',
@@ -128,7 +153,8 @@ $articles = [
         'title' => 'Pickles to cause cancer, new study claims',
         'published_date' => '2020-10-19 14:12',
         'content' => 'Scientists at Eslöv University has, after doing some science, found
-        that cucumbers might cause different forms of cancer.',
+        that cucumbers might cause different forms of cancer. "-Bostongurka and all of that is
+        fine, but fresh cucumber is to be avoided", head scientist Johan Sebastian Hasch says.',
 
         'id' => 5,
         'likes' => '55',
