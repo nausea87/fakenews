23c11f7eedd75dc1557ef108d8cb3c59984baea4
Erik <62840687+nausea87@users.noreply.github.com>
2020-11-01 12:40:30 +0100

    Final Edits with comments, Sunday 01/11

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ function getArticleById(array $articles, int $id): array
     foreach ($articles as $article) {
         if ($article['id'] === $id) {
             return $article;
-        } /* Creating article $id from ['id'] and calling the variable $id in index.php */
+        } /* Creating article $id from ['id'] and calling the variable '$id' in index.php */
     }
 }
 
@@ -29,6 +29,6 @@ function shortenText(string $text, $chars = 300): string
     $text = substr($text, 0, strrpos($text, ' '));
     $text = $text . "...";
     return $text;
-    /* Might remove temporary due to button not fixed as I want yet */
+    /* Remove temporary due to button not fixed as I want yet */
     /* Function to be implemented again after learning more. */
 }
